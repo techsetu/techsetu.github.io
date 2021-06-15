189c16ec3c52cdeefb1cdb39a542bf03a62d0fc5
Cool BRG <budhram.gurung01@gmail.com>
2021-06-15 16:54:05 +0530

    Fixed form apply behavior

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,13 @@
 /*Custom JavaScript*/
+
+$(function() {
+  // google form submit iframe behavior
+  var iframeLoadCount = 0;
+  $('#apply').on('load', function () {
+    console.log("Sure load event is called!");
+    iframeLoadCount += 1;
+    if (iframeLoadCount > 1) {
+      $('#apply').height("380px");
+    }
+  });
+});
